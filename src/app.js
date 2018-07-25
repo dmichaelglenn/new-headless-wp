735a604f1896b00b765a01a1b269dfc357709f96
Michael Glenn <michael@valentinecreative.co>
2018-07-24 21:30:13 -0700

    finishing add router basic

--- src/app.js
+++ src/app.js
@@ -1,33 +1,27 @@
 import Api from './api';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PostList from './post-list';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import { PostList, Post }from './post-list';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            posts: []
-        };
-    }
 
-    componentDidMount() {
-        let api = new Api();
-
-        api.posts().then(data => {
-            this.setState({
-                posts: data
-            });
-        });
-    }
     render() {
         return (
             <div className="container">
                 <h1>Hello Headless BOI</h1>
                 <PostList posts={this.state.posts} />
+                <Switch>
+                    <Route exact path="/" component={PostList} />
+                    <Route path="/post/:id" component={Post} />
+                </Switch>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app-container'))
+ReactDOM.render(
+    <HashRouter>
+         <App />     
+    </HashRouter>
+    , document.getElementById('app-container'))
--- src/post-list.js
+++ src/post-list.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-function Post(props) {
+class Post extends React.Component {
+    
+    render() {
     return (
-        <div className="row">
-            <h3>{props.title.rendered}</h3>
-            <div dangerouslySetInnerHTML={{__html: props.content.rendered}} />
-        </div>
-    )
+            <div className="row">
+                <h3>{props.title.rendered}</h3>
+                <div dangerouslySetInnerHTML={{__html: props.content.rendered}} />
+            </div>
+        );
+    }
 }
-    export default function PostList(props) {
-        let posts = props.posts.map((post, index) => <Post key={index} {...post} />);
+  class PostList extends React.Component {
+      constructor() {
+          super();
+          this.state = {
+              posts: []
+          };
+      }
+
+      componentDidMount() {
+          let api = new Api();
+
+          api.posts().then(data => {
+              this.setState({
+                  posts: data
+              });
+          });
+      }
+
+      render() {
+        let posts = this.state.posts.map((post, index) => 
+        <h3 key={index}>
+            <Link to={'/post/' + post.id}>{post.title.rendered}</Link>
+        </h3>
+        );
         return (
             <div>{posts}</div>
         );
+      }
     }
+
+    export {Post, PostList};
